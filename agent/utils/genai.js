963d4a27eb08e67eceaaf30a4570988cc2c796d8
abhi-ananthu <abhiramajithr@gmail.com>
2025-07-25 22:41:58 +0530

    added model, langchai

---
+++ agent/utils/genai.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+const { ChatGoogleGenerativeAI } = require('@langchain/google-genai');
+
+const model = new ChatGoogleGenerativeAI({
+  model: 'gemini-2.5-flash',
+  maxOutputTokens: 2048,
+  apiKey: process.env.GOOGLE_API_KEY, // add your API key here
+});
+
+module.exports = model;
